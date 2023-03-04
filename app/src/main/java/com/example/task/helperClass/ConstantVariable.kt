c90ae4a86d7d5252d27beb29ca576bbeb967eac5
RavinaSanghani <70128902+RavinaSanghani@users.noreply.github.com>
2023-03-04 13:38:04 +0530

    Token Change
    Token Change

--- app/src/main/java/com/example/task/helperClass/ConstantVariable.kt
+++ app/src/main/java/com/example/task/helperClass/ConstantVariable.kt
@@ -9,7 +9,7 @@ class ConstantVariable {
         const val keyAuthorization = "Authorization"
         const val keyGithubVersion = "X-GitHub-Api-Version"
         const val keyAcceptGithub = "application/vnd.github+json"
-        const val keyAuthorizationToken = "Bearer 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"
+        const val keyAuthorizationToken = "Bearer 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"
         const val keyGithubVersionDate = "2022-11-28"
     }
 }
